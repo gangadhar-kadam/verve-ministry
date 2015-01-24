c3befa83fd2d95c9b5fcfdb06968f2cf968b18d7
gangadhar kadam <gangadhar.k@gmail.com>
2015-01-24 11:13:22 +0530

    Initial commit.

---
+++ hooks.py
@@ -0,0 +1,107 @@
+app_name = "church_ministry"
+app_title = "Church Ministry"
+app_publisher = "New Indictrans Technologies Pvt. Ltd."
+app_description = "This app id for manage church ministry"
+app_icon = "icon-book"
+app_color = "#589494"
+app_email = "gangadhar.k@indictranstech.com"
+app_url = "info@indictranstech.com"
+app_version = "0.0.1"
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/church_ministry/css/church_ministry.css"
+# app_include_js = "/assets/church_ministry/js/church_ministry.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/church_ministry/css/church_ministry.css"
+# web_include_js = "/assets/church_ministry/js/church_ministry.js"
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+#	"Role": "home_page"
+# }
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# Installation
+# ------------
+
+# before_install = "church_ministry.install.before_install"
+# after_install = "church_ministry.install.after_install"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "church_ministry.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+#	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"church_ministry.tasks.all"
+# 	],
+# 	"daily": [
+# 		"church_ministry.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"church_ministry.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"church_ministry.tasks.weekly"
+# 	]
+# 	"monthly": [
+# 		"church_ministry.tasks.monthly"
+# 	]
+# }
+
+# Testing
+# -------
+
+# before_tests = "church_ministry.install.before_tests"
+
+# Overriding Whitelisted Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "church_ministry.event.get_events"
+# }
+
